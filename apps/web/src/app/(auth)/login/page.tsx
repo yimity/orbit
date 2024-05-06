8c1674022c0d6be495b7ae8f574d4ad52e5afe80
Andrew.Liang <andrew.liang@developtools.com>
2024-05-06 10:17:33 +0900

    feat(web): add some style
    add icons and style login page.

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
 import { env } from '@orbitkit/env/web/server';
+import { Button } from '@orbitkit/ui/button';
 
 const googleAuthIsEnabled =
   env.AUTH_GOOGLE_ID !== undefined &&
@@ -12,12 +15,31 @@ const githubAuthIsEnabled =
 
 export default function Page() {
   return (
-    <main className="container mx-auto flex flex-col">
+    <main className="container mx-auto flex flex-col items-center w-100">
+      <div className={'mt-20'}>
+        <img
+          className="w-auto h-7 relative block dark:hidden"
+          src={
+            'https://mintlify.s3-us-west-1.amazonaws.com/orbitkit/logo/orbitkit-light.svg'
+          }
+          alt={'orbitKit'}
+        />
+      </div>
       {githubAuthIsEnabled && (
-        <Link href="/login/github">Sign in with Github</Link>
+        <Button className={'w-64 my-10'} asChild>
+          <div>
+            <GitHubLogoIcon className="mr-2 h-4 w-4" />{' '}
+            <Link href="/login/github">Sign in with Github</Link>
+          </div>
+        </Button>
       )}
       {googleAuthIsEnabled && (
-        <Link href="/login/google">Sign in with Google</Link>
+        <Button className={'w-64'} asChild>
+          <div>
+            <GitHubLogoIcon className="mr-2 h-4 w-4" />{' '}
+            <Link href="/login/google">Sign in with Google</Link>
+          </div>
+        </Button>
       )}
 
       {!githubAuthIsEnabled && !googleAuthIsEnabled && (
